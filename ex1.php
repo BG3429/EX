6ce4e43548d06ad2ec53282f2493fb49f9c5027c
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2018-02-21 17:20:20 +0100

    V2

--- ex1.php
+++ ex1.php
@@ -4,8 +4,14 @@
 <meta charset="UTF-8">
 <title>EX1</title>
 </head>
+	<br><a href="?message=Hello">Afficher Hello</a>
+	<br><a href="?message=Salut">Afficher Salut</a>
+	<br><a href="?message=Coucou">Afficher Coucou</a>
 <body>
+
 <?php
+$message=$_GET["message"];
+	echo "<br>{$message}";
 
 ?>
 </body>
