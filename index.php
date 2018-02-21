4b11dfed671e69202ecc5476a7d6ca58bb8fa5ac
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2018-02-21 16:33:39 +0100

    v1

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,18 +11,3 @@ echo "salut";
 ?>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>EX0</title>
-</head>
-<body>
-<?php
-echo "Hello world!";
-echo "salut";
-?>
-</body>
-</html>
->>>>>>> 2080d16dfa484159889cd911e2f56d3f207d3ffb
