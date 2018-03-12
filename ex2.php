0cbe0992cc95df835f82570d25f058505147c4b8
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2018-03-12 10:22:03 +0100

    V2

--- ex2.php
+++ ex2.php
@@ -4,17 +4,27 @@
 <title>EX2</title>
 </head>
 
+<body>
+
 <form method="GET">
     <label for="size">Size : </label>
     <input type="number" value="" name="size" id="size">
+	<label for="color">Color (ex:FF0000): </label>
+    <input type="text" value="" name="color" id="color">
     <input type="submit" value="Valider">
 </form>
 
-<body>
-
 <?php
+if(isset($_GET['size'])){
+	$size=$_GET["size"];
+	if(isset($_GET['color'])){
+		$color=$_GET["color"];
+		echo "<p style='color:#{$color};'><span style='font-size :{$size}px'><b>Message de taille {$size}px</b> </span></p>";
+}}
 
-echo "<div style='font-size: {$size}px;'>Message de taille {$size}px en rouge</div>";
+else{
+	echo "<p style=color:red;><span style='font-size :30px'><b> Erreur</b> </span></p>";
+}
 
 ?>
 </body>
